2c67b7b68f23131768937c5ea1b1523a02ae860b
han <consistant1y@ajou.ac.kr>
2022-03-04 18:59:15 +0900

    feat: add header & add docstring & fix bugs
    - add header for string print
    - add docstring for each reduce & accuracy
    - change sum to sum * n

--- metrics.py
+++ metrics.py
@@ -4,8 +4,8 @@ import torch
 from torch import distributed as dist
 
 
-class BaseMetric:
-    def __init__(self, reduce_every_n_step=50, reduce_on_compute=False, fmt='{val:.2f} ({avg:.2f})'):
+class Metric:
+    def __init__(self, reduce_every_n_step=50, reduce_on_compute=False, header='', fmt='{val:.2f} ({avg:.2f})'):
         """Base Metric Class supporting ddp setup
 
         :arg
@@ -27,11 +27,12 @@ class BaseMetric:
         self.sum = None
         self.avg = None
         self.n = None
+        self.header = header
         self.fmt = fmt
 
     def update(self, val, n=1):
         if isinstance(val, torch.Tensor):
-            val = val.detach()
+            val = val.detach().clone()
         elif self.reduce_every_n_step and not isinstance(val, torch.Tensor):
             raise ValueError('reduce operation is allowed for only tensor')
 
@@ -42,7 +43,7 @@ class BaseMetric:
             self.avg = self.sum / self.n
         else:
             self.val = val
-            self.sum += val
+            self.sum += val * n
             self.n += n
             self.avg = self.sum / self.n
 
@@ -64,22 +65,41 @@ class BaseMetric:
         self.avg = None
 
     def __str__(self):
-        return self.fmt.format(**self.__dict__)
+        return self.header + ' ' + self.fmt.format(**self.__dict__)
 
 
 def Accuracy(y_hat, y, top_k=(1,)):
+    """Compute top-k accuracy
+    :arg
+        y_hat(tensor): prediction shaped as (B, C)
+        y(tensor): label shaped as (B)
+        top_k(tuple): how exactly model should predict in each metric
+
+    :return
+        list of metric scores
+    """
     prediction = torch.argsort(y_hat, dim=-1, descending=True)
-    accuracy = [(prediction[:, :k] == y.unsqueeze(-1)).float().sum(dim=-1).mean() for k in top_k]
+    accuracy = [(prediction[:, :min(k, y_hat.size(1))] == y.unsqueeze(-1)).float().sum(dim=-1).mean() * 100 for k in top_k]
     return accuracy
 
 
 def all_reduce_mean(val, world_size):
+    """Collect value to each gpu
+    :arg
+        val(tensor): target
+        world_size(int): the number of process in each group
+    """
     dist.all_reduce(val.detach(), dist.ReduceOp.SUM)
     val = val / world_size
     return val
 
 
 def reduce_mean(val, world_size):
+    """Collect value to local zero gpu
+    :arg
+        val(tensor): target
+        world_size(int): the number of process in each group
+    """
     dist.reduce(val.detach(), 0, dist.ReduceOp.SUM)
     val = val / world_size
     return val
