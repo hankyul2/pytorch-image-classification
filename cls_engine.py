75fba4ce7ac6e91bfb4d794cc7d9314931c3ffc6
han <consistant1y@ajou.ac.kr>
2022-03-11 10:22:05 +0900

    refactor: metric logger style in validation
    - change metric logger stype in validation

--- cls_engine.py
+++ cls_engine.py
@@ -1,3 +1,4 @@
+import datetime
 import time
 
 import torch
@@ -7,13 +8,18 @@ from metrics import Metric, Accuracy
 
 @torch.inference_mode()
 def validate(model, criterion, valid_dataloader, args):
-    model.eval()
-    data_m = Metric(reduce_every_n_step=args.print_freq, header='data')
-    batch_m = Metric(reduce_every_n_step=args.print_freq, header='batch')
-    top1_m = Metric(reduce_every_n_step=args.print_freq, header='top-1 acc')
-    top5_m = Metric(reduce_every_n_step=args.print_freq, header='top-5 acc')
-    loss_m = Metric(reduce_every_n_step=args.print_freq, header='loss')
 
+    # 1. create metric
+    data_m = Metric(reduce_every_n_step=args.print_freq, header='Data:')
+    batch_m = Metric(reduce_every_n_step=args.print_freq, header='Batch:')
+    top1_m = Metric(reduce_every_n_step=args.print_freq, header='Top-1:')
+    top5_m = Metric(reduce_every_n_step=args.print_freq, header='Top-5:')
+    loss_m = Metric(reduce_every_n_step=args.print_freq, header='Loss:')
+
+    # 2. start validate
+    args.log("")
+    args.log("Start Validation...")
+    model.eval()
     if args.channels_last:
         model = model.to(memory_format=args.channels_last)
 
@@ -28,7 +34,7 @@ def validate(model, criterion, valid_dataloader, args):
             x = x.to(memory_format=args.channels_last)
             y = y.to(memory_format=args.channels_last)
 
-        batch_m.update(time.time() - start_time)
+        data_m.update(time.time() - start_time)
 
         with torch.cuda.amp.autocast(args.amp):
             y_hat = model(x)
@@ -40,21 +46,32 @@ def validate(model, criterion, valid_dataloader, args):
         top5_m.update(top5)
         loss_m.update(loss)
 
-        if batch_idx and args.print_freq and args.print_freq % batch_idx == 0:
-            args.log(f"[{batch_idx}/{total_iter}] {batch_m} {data_m} {loss_m} {top1_m} {top5_m}")
+        if batch_idx and args.print_freq and batch_idx % args.print_freq == 0:
+            num_digits = len(str(total_iter))
+            args.log(f"VALIDATION: [{batch_idx:>{num_digits}}/{total_iter}] {batch_m} {data_m} {loss_m} {top1_m} {top5_m}")
 
         batch_m.update(time.time() - start_time)
         start_time = time.time()
 
+    # 3. calculate metric
+    batch = batch_m.compute()
+    duration = datetime.timedelta(seconds=batch_m.sum)
+    data = data_m.compute()
     top1 = top1_m.compute()
     top5 = top5_m.compute()
     loss = loss_m.compute()
-    batch = batch_m.compute()
-    data = data_m.compute()
 
-    args.log(f"{batch_m} {data_m} {loss_m} {top1_m} {top5_m}")
+    # 4. print metric
+    space = 15
+    num_metric = 6
+    args.log('-'*space*num_metric)
+    args.log(("{:>15}"*num_metric).format('Duration', 'Batch', 'Data', 'F+B+O', 'Top-1 Acc', 'Top-5 Acc'))
+    args.log('-'*space*num_metric)
+    args.log(f"{str(duration).split('.')[0]:>15}{batch:15.4f}{data:>15.4f}{batch-data:>15.4f}{top1:15.4f}{top5:15.4f}")
+    args.log('-'*space*num_metric)
 
-    return top1, top5, loss, batch, data
+    return batch, data, top1, top5, loss
+
 
 
 def train_one_epoch(model, ema_model, ddp_model, optimizer, scheduler, criterion, scaler, args):
