2c21a501b92c8dc30bf045373596ffbed6e43d36
han <consistant1y@ajou.ac.kr>
2022-04-10 13:47:48 +0900

    chore: fix package
    - remove find_packages

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.0.2',
     description='pytorch image classification',
     url='https://github.com/hankyul2/pytorch-image-classification',
-    packages=find_packages(exclude=['tests']),
+    packages=['pic'],
     author='hankyul',
     author_email='consistant1y@ajou.ac.kr',
 )
