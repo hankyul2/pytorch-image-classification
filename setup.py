ad3cae13c90c03ad6a5af332cf776146bd52e137
han <consistant1y@ajou.ac.kr>
2022-04-10 13:35:43 +0900

    fix: no packages found bugs
    - add find_packages

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='pic',
     version='0.0.1',
     description='pytorch image classification',
     url='https://github.com/hankyul2/pytorch-image-classification',
-    packages=['pic'],
+    packages=find_packages(exclude=['tests']),
     author='hankyul',
     author_email='consistant1y@ajou.ac.kr',
 )
