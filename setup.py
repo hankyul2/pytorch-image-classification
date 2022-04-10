5567a8385b927f33348ae468a2ab91bb8be30b31
han <consistant1y@ajou.ac.kr>
2022-04-10 13:43:37 +0900

    chore: update version
    - change version to 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pic',
-    version='0.0.1',
+    version='0.0.2',
     description='pytorch image classification',
     url='https://github.com/hankyul2/pytorch-image-classification',
     packages=find_packages(exclude=['tests']),
