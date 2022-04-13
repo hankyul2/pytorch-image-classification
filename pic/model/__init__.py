b22618038acad5b06b5ba1de27552091a64b3b76
han <consistant1y@ajou.ac.kr>
2022-04-14 08:48:58 +0900

    fix: SKNet is not found
    - add import statement in __init__ in model

--- pic/model/__init__.py
+++ pic/model/__init__.py
@@ -3,9 +3,10 @@ try:
     from .sunet import SUNet
 except:
     pass
+from .alexnet import AlexNet
 from .resnet import ResNet
 from .res2net import Res2Net
-from .alexnet import AlexNet
+from .sknet import SKNet
 from .ema import ModelEmaV2
 from .register import create_model, get_argument_of_model, register_model
 from .factory import get_model, get_ema_ddp_model
