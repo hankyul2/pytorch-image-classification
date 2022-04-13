34c9b7722ceda5d56cb76479cd331ff1ed1d4204
han <consistant1y@ajou.ac.kr>
2022-04-13 12:06:25 +0900

    feat: support selective kernel network now
    - add sknet(*)_32c_4w model which have similar the number of parameters as resnext(*) series

---
+++ tests/model/test_sknet.py
@@ -0,0 +1,20 @@
+import torch
+from torch import nn
+
+from pic.model import create_model
+from pic.model.sknet import SelectiveKernel
+
+
+def test_selective_kernel_forward():
+    x = torch.rand([2, 512, 28, 28])
+    sk_conv = SelectiveKernel(512, 32, 2, nn.BatchNorm2d)
+    y = sk_conv(x)
+    assert list(y.shape) == [2, 512, 14, 14]
+
+
+def test_sknet_forward():
+    x = torch.rand([2, 3, 224, 224])
+    model = create_model('sknet26_32_4')
+    y = model(x)
+    assert list(y.shape) == [2, 1000]
+
